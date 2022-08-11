68310a1fd0700bd35f6a36c60c3b5e3e8e1de7f5
Stephen Wong <wongstephenky@gmail.com>
2022-08-10 18:06:09 -0700

    kinda works

--- app.js
+++ app.js
@@ -1,21 +1,74 @@
 require("dotenv").config();
+const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
-const PORT = 8800;
 const cors = require("cors");
+const PORT = 8800;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const obj = {};
 const scrapeProduct = async (url) => {
-  const browser = await puppeteer.connect({
-    browserWSEndpoint:
-      `wss://chrome.browserless.io?token=` + process.env.API_KEY,
-  });
-  //new page
+  let browser = null;
+  try {
+    browser = await puppeteer.connect({
+      browserWSEndpoint:
+        `wss://chrome.browserless.io?token=` + process.env.API_KEY,
+      headless: true,
+    });
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    let html = await page.evaluate(() => document.body.innerHTML);
+    const $ = await cheerio.load(html);
+
+    await $("#productTitle", html).each(function () {
+      let title = $(this).text();
+      obj.title = title.trim();
+    });
+
+    await $(".attach-base-product-price", html).each(function () {
+      let price = $(this).val();
+      obj.price = price;
+    });
+
+    //description point 1
+    const [el5] = await page.$x('//*[@id="feature-bullets"]/ul/li[2]');
+    if (!el5) {
+      description1 = "";
+    } else {
+      const txt4 = await el5.getProperty("textContent");
+      description1 = await txt4.jsonValue();
+    }
+    //description point 2
+    const [el6] = await page.$x('//*[@id="feature-bullets"]/ul/li[3]/span');
+    if (!el6) {
+      description2 = "";
+    } else {
+      const txt5 = await el6.getProperty("textContent");
+      description2 = await txt5.jsonValue();
+    }
+    obj.description = description1 + " " + description2;
+
+    const [el] = await page.$x(`//*[@id="landingImage"]`);
+    const src = await el.getProperty("src");
+    obj.image = await src.jsonValue();
+
+    return obj;
+  } catch (err) {
+    console.log("error " + err);
+  } finally {
+    if (browser) {
+      browser.close();
+    }
+  }
+
+  /*   //new page
   const page = await browser.newPage();
   await page.goto(url);
   //title
@@ -36,7 +89,7 @@ const scrapeProduct = async (url) => {
     price = 0;
   } else {
     const txt2 = await el3.getProperty("textContent");
-    price = await txt2.jsonValue();
+    const price = awaittxt2.jsonValue();
   }
   //orig $$
   const [el4] = await page.$x(
@@ -46,7 +99,7 @@ const scrapeProduct = async (url) => {
     orig = 0;
   } else {
     const txt3 = await el4.getProperty("textContent");
-    orig = await txt3.jsonValue();
+    const orig = await txt3.jsonValue();
   }
 
   //description point 1
@@ -67,13 +120,22 @@ const scrapeProduct = async (url) => {
   }
   const description = description1 + " " + description2;
   browser.close();
-  return { title, image, description, orig, price };
+  return { title, image, description, orig, price }; */
 };
 
 app.get("/new/*", async (req, res) => {
   try {
     const url = req.params[0];
     const data = await scrapeProduct(url);
+    res.json(data);
+  } catch (err) {
+    res.send(console.log("error " + err));
+  }
+});
+
+app.post("/scrape/", async (req, res) => {
+  try {
+    const data = await scrapeProduct(req.body.url);
     res.json(data);
   } catch (err) {
     res.send(console.log("error " + err));
